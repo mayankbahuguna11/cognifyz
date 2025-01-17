e1cc791a8a4659e190193a7741c921caa625b141
mayankbahuguna11 <bahugunamayank23@gmail.com>
2025-01-17 23:42:36 +0530

    Add files via upload

---
+++ Calculator_6/script.js
@@ -0,0 +1,7 @@
+document.getElementById("addButton").onclick = function() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const sum = num1 + num2;
+    document.getElementById("result").textContent = "Result: " + sum;
+  }
+  
